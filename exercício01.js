bd60e571d63c3e9c9a225653bae444291400ebe4
Giovanna Alba <giovannaalba.tds1.senai@gmail.com>
2024-08-15 09:12:01 -0300

    primeiro exercício

---
+++ exercício01.js
@@ -0,0 +1,74 @@
+let livro = "É assim que acaba";
+
+const livroObjeto = new Object()
+livroObjeto.autor = "Colleen Hoover";
+livroObjeto.titulo = "É assim que acaba";
+livroObjeto.ano = 2017;
+
+livroObjeto.ano = 2018; //Mudando para o ano certo
+
+console.log(livro);
+console.log(livroObjeto);
+
+
+let aluno = "Giovanna";
+
+const alunoObjeto = new Object()
+alunoObjeto.nome = "Giovanna";
+alunoObjeto.turma = "tds1";
+alunoObjeto.chamada = 15
+
+alunoObjeto.chamada = 17; // Corrigindo
+
+console.log(aluno);
+console.log(alunoObjeto);
+
+let carro = "Caminhonete";
+
+const carroObjeto = new Object()
+carroObjeto.cor = "Preto";
+carroObjeto.modelo = "Volkswagen Amarok Highline V6 3.0 Turbo Diesel";
+carroObjeto.ano = 2019;
+
+carroObjeto.ano = 2018;
+
+console.log(carro);
+console.log(carroObjeto);
+
+
+let filme = "Harry Potter";
+
+const filmeObjeto = new Object()
+filmeObjeto.titulo = "Harry Potter: Prisioneiro de Azkaban";
+filmeObjeto.diretor = "J. K. Rowling";
+filmeObjeto.ano = 2004;
+
+filmeObjeto.diretor = "Alfonso Cuarón";
+
+console.log(filme);
+console.log(filmeObjeto);
+
+
+let cachorro = "Bruce";
+
+const cachorroObjeto = new Object()
+cachorroObjeto.nome = "Bruce";
+cachorroObjeto.raça = "pug";
+cachorroObjeto.cor = "preto";
+
+cachorroObjeto.cor = "abricot";
+
+console.log(cachorro);
+console.log(cachorroObjeto);
+
+let produto = "maquiagem";
+
+const produtoObjeto = new Object()
+produtoObjeto.descrição = "batom";
+produtoObjeto.marca = "Dior";
+produtoObjeto.preço = 150.00;
+
+produtoObjeto.preço = 269.00;
+
+console.log(produto);
+console.log(produtoObjeto);
